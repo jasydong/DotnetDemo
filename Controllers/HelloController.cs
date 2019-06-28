f1f1b75a3e6b1548c561ee780de1c650465638c0
jasydong <jasydong@qq.com>
2019-06-28 22:20:41 +0800

    添加Hello控制器

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetDemo.Controllers
+{
+    [Route("demo/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET demo/hello
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello", "Dotnet" };
+        }
+
+        // GET demo/hello/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Hello, Dotnet!";
+        }
+
+        // POST demo/hello
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
